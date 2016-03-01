a522f9b3eb6910fe4601e2c4dbfd2775938217df
selichter <lichter.sarahe@gmail.com>
2016-03-01 09:46:27 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,18 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+    end
+
+    body_hash = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @items = body_hash["items"]
+    else
+      @error = body_hash["message"]
+    end
+
+    render 'search'
   end
 end
