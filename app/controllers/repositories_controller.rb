f68ae11f6ac62c501af3f4a04d8ba059b94f5318
PJ Wickwire <pjpeterww@gmail.com>
2016-03-24 14:46:20 -0400

    removed info

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,8 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get('https://api.github.com/search/repositories')do |req|
-      req.params['client_id'] = '9bf23011356a07082e79'
-      req.params['client_secret'] = '41a8ac8809b5a83d581272c1fc1d9bc84d7b7bc4'
+      req.params['client_id'] = 'id'
+      req.params['client_secret'] = 'key'
       req.params['q'] = params[:query]
       #req.params['sort'] = params[:zipcode]
       #req.params['order'] = 'coffee shop'
