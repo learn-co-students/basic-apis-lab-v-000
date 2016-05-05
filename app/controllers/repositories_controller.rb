5656a1385862b9ce2a3334caf8a8701c82c1c3b3
authorbeard <msnolan@gmail.com>
2016-05-05 15:11:51 -0400

    all specs passing

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,25 @@
 class RepositoriesController < ApplicationController
+  
 
   def search
 
   end
 
   def github_search
+    @resp=Faraday.get "https://api.github.com/search/repositories" do |req|
+      req.params['q']=params[:query]
+      # req.params['client_id']=Figaro.env.github_app_id
+      # req.params['client_secret']=Figaro.env.github_app_secret
+    end
+    # --name, link, description
+    body = JSON.parse(@resp.body)
+    ## Need the array with key "items"
+
+    @details=body["items"].map{|i| {"name"=>i["name"], "html_url"=>i["html_url"], "description"=>i["description"]} }
+
+    render 'search'
   end
+
+
+
 end
