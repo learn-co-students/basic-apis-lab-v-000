1314b89902d088b16eb1b038f6bbf0b52421b08e
Dakota Martinez <dakotaleemusic@gmail.com>
2016-05-12 22:19:16 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,19 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin 
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['q'] = params[:query]
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @repositories = body["items"]
+      else
+        @errors = body["errors"]
+      end
+    rescue Faraday::TimeoutError
+    
+    end
+    render 'search'
   end
 end
