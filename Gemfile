d9310ea44614163659606b67fc88ceeffd806045
Jesse Novotny <jessenovotny@gmail.com>
2016-10-19 16:59:18 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'faraday'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry'
   gem 'rspec-rails'
   gem 'capybara'
   gem 'shoulda-matchers'
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,18 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'dbaecd06e6bb45a4313b'
+      req.params['client_secret'] = '8162304c622e76d5cc01fcb2d9825321ea08af05'
+      # req.params['v'] = '3'
+      req.params['q'] = params[:query]
+    end
+    body = JSON.parse(@resp.body)
+    # binding.pry
+    if @resp.success?
+      @repositories = body["items"]
+    end
+    # binding.pry
+    render 'repositories/search'
   end
 end
