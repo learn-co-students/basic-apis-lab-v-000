c75cad4ac145cceb0c30a0a6e15ea6ee6bf89fb3
tpstar <hanlee12@gmail.com>
2016-11-15 23:47:34 -0500

    cannot load dtrupenn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,19 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'e47847cc903306341302'
+      req.params['client_secret'] = 'a69126877f2e72e6a4cc6ca7065e101b934f9d5d'
+      req.params['q'] = '#{params[:query]}'
+    end
+    # binding.pry
+    body_hash = JSON.parse(@resp.body)
+    if @resp.success?
+      @items = body_hash["items"]
+    # else
+    #   binding.pry
+    end
+    # binding.pry
+    render 'search'
   end
 end
