1d06e15bdb8557c30070fa5675effe3b2a07f93a
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2016-11-16 16:02:26 -0500

    code works but does not pass the test

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,9 +8,8 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = 'e47847cc903306341302'
       req.params['client_secret'] = 'a69126877f2e72e6a4cc6ca7065e101b934f9d5d'
-      req.params['q'] = '#{params[:query]}'
+      req.params['q'] = params[:query]+'+user:dtrupenn'
     end
-    # binding.pry
     body_hash = JSON.parse(@resp.body)
     if @resp.success?
       @items = body_hash["items"]
