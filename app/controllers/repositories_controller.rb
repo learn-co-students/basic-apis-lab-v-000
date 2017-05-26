51004b0abe72763e89580d98f5ebc999cd4a41f9
Ray Mulligan <rmullig2@optonline.net>
2017-05-26 15:11:01 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,20 @@
 class RepositoriesController < ApplicationController
 
   def search
-
+    
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'a671d476a0cf0e74c716'
+      req.params['client_secret'] = '6ea00dcf62cb80d8a8a2d72081b353b581f92b47'
+      req.params['q'] = params[:query]
+      req.params['per_page'] = '100'
+    end
+    
+    body = JSON.parse(@resp.body)
+    @repos = body["items"]
+    #binding.pry
+    render 'search'
   end
 end
