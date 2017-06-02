e7fe1816fb79254fb5c62255e0ff7c895c7968dc
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-02 12:33:44 -0600

    completed basic api lab

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,17 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = '70e91f89aac5a4c9ed23'
+      req.params['client_secret'] = '5e3187361fd1b4706cff329eda93195c85bd713f'
+      req.params['q'] = params[:query]
+    end
+    body = JSON.parse(@resp.body)
+    @repos = body['items']
+    render 'search'
   end
+
 end
