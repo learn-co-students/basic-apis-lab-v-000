c73a1f857dcf73ec0ad44d6944aff5cd0f5b56d1
christinechoi <smi77y@gmail.com>
2017-06-25 09:43:50 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,18 +9,9 @@ class RepositoriesController < ApplicationController
       req.params['client_id'] = '458d3cab6771948d4bd2'
       req.params['client_secret'] = 'f0ca9679247222a4079d93e95c0adedaea6cb493'
       req.params['q'] = params[:query]
-      
     end
-    # binding.pry
     body_hash = JSON.parse(@resp.body)
     @items = body_hash["items"]
-
-    @name = body_hash["items"][0]["name"]
-    # binding.pry
-    @description = body_hash["items"][0]["description"]
-    @url = body_hash["items"][0]["url"]
-    # binding.pry
     render 'search'
-
   end
 end
