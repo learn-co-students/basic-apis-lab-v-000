9f1e8b80de97a48ab93c143948b8499df5b7494a
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-13 11:28:35 -0400

    Rescue Timeout Error in Repositories Controller.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,16 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params[:q] = params[:query]
-      req.params[:client_id] = '653c4c474c057fa03623'
-      req.params[:client_secret] = '7d592224ee90e7e218ef7213146d3d1d031687b1'
-    end
-    body = JSON.parse(@resp.body)
-    if @resp.success?
-      @items = body["items"]
-    else
-      @error = body["message"]
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params[:q] = params[:query]
+        req.params[:client_id] = '653c4c474c057fa03623'
+        req.params[:client_secret] = '7d592224ee90e7e218ef7213146d3d1d031687b1'
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @items = body["items"]
+      else
+        @error = body["message"]
+      end
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
     end
     render 'search'
   end
