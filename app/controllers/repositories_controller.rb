91a05662825b74b21d4ffefd3341eaefcf11e081
nicholasbair <bairinc@gmail.com>
2017-07-14 18:04:05 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,21 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      @res = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['client_id'] = ENV['CLIENT_ID']
+        req.params['client_secret'] = ENV['CLIENT_SECRET']
+        req.params['q'] = params['query']
+      end
+      body = JSON.parse(@res.body)
+      if @res.success?
+        @results = body["items"]
+      else
+        @error = 'Something happened and its bad.'
+      end
+    rescue
+      @error = 'Something happened and its bad.'
+    end
+    render 'search'
   end
 end
