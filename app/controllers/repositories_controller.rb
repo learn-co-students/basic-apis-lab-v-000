70e212f5a06c4c4db4a2b69a0d33991de491baaa
Howard DeVennish <howarddevennish@gmail.com>
2017-08-06 14:08:39 -0400

    add .env and dotenv gem to remove tokens from code

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,8 +8,10 @@ class RepositoriesController < ApplicationController
     begin
       resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['q'] = params[:query]
-        req.params['client_id'] = '93a740f8bce1809be531'
-        req.params['client_secret'] = 'fb78fcafed243d76d68cec788d954593e1e01c3d'
+        req.params['client_id'] = ENV['CLIENT_ID']
+        req.params['client_secret'] = ENV['CLIENT_SECRET']
+        # req.params['client_id'] = '93a740f8bce1809be531'
+        # req.params['client_secret'] = 'fb78fcafed243d76d68cec788d954593e1e01c3d'
       end
 
       body = JSON.parse(resp.body)
