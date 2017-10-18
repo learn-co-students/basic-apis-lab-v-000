53a1cae5aa227e38dbf74df76b7bf48492be5ea6
mikka <mikkanthrope@gmail.com>
2017-10-18 13:42:18 -0700

    Done.

--- app/controllers/repositories_controller_original.rb
+++ app/controllers/repositories_controller_original.rb
@@ -5,16 +5,15 @@ class RepositoriesController < ApplicationController
 
   def github_search
     begin
-    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+    @resp = Faraday.get 'https://api.github.com/users/squirrelnest/repos' do |req|
       req.params['client_id'] = 'Iv1.4152657178dd52b8'
       req.params['client_secret'] = '3c1c1dd996aaf614ff68203a876f4c0a6603189c'
-      req.params['q'] = params['query']
     end
     body = JSON.parse(@resp.body)
     if @resp.success?
-      @repos = body["items"]
+      @repos = body
     else
-      @error = body["message"]
+      @error = body["meta"]["errorDetail"]
     end
 
     rescue Faraday::ConnectionFailed
