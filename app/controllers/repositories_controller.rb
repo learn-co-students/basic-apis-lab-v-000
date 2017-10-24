99e8bfe796c993464ce6122cde4852c321032908
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2017-10-24 11:36:00 -0500

    Update repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,8 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get "https://api.github.com/search/repositories" do |req|
-      req.params['client_id'] = 'Iv1.cc372eddc007c796'
-      req.params['client_secret'] = '94b3a7542bbd8472a9ef321af0d7272de6cbc19b'
+      req.params['client_id'] = ''
+      req.params['client_secret'] = ''
       req.params['q'] = params[:query]
     end
     #binding.pry
