dd179228c1a1effe7485f4ec54440190645a03c5
alecgalba <alecgalba@gmail.com>
2017-11-03 11:09:38 -0400

    took out the id and secret before submitting

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,17 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'client_id'
+      req.params['client_secret'] = 'client_secret'
+      req.params['sort'] = 'updated'
+      req.params['order'] = 'desc'
+      req.params['q'] = params[:query]
+    end
+
+    body = JSON.parse(resp.body)
+
+    @results = body['items']
+    render 'search'
   end
 end
