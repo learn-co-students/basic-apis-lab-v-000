8b82cccbfab26e2e9a6482cb3681945504b9b937
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-29 23:00:25 -0800

    tests pass

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,16 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    @resp = Faraday.get "https://api.github.com/search/repositories" do |req|
+      req.params['client_id'] = ENV['CLIENT_ID']
+  		req.params['client_secret'] =ENV['CLIENT_SECRET']
+  		req.params['q'] = params[:query]
+    end
+    body = JSON.parse(@resp.body)
+    @projects = body["items"]
+    render 'search'
   end
 end
