56bd246848ecda7f2692da9ed41e17020acab3fa
kyleblee <hello.kylelee@gmail.com>
2018-01-13 17:05:01 -0500

    lab complete

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params["q"] = params["query"]
+    end
+    @repos = JSON.parse(@resp.body)["items"]
+    render 'search'
   end
 end
