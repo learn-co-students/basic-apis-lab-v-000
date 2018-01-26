5d7eb8ae6d4263d931fe61c2ae32a3017c8e212a
Cynthia Rittenbach <crittenbach@gmail.com>
2018-01-26 14:05:40 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'pry'
 
+gem 'faraday'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,4 @@
+
 class RepositoriesController < ApplicationController
 
   def search
@@ -5,5 +6,15 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    #binding.pry
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+     req.params['client_id'] = 'N1WA1KZL2EDDKSMVGSPPI5F5H21DJBPBKUKPOFYWMK3BKJQR'
+     req.params['client_secret'] = '00QJAMOOEELIYWJHQWD2J5ARGMLELVTUGYVDC4RBAYOGRLJB'
+     req.params['q'] = params[:query]
+    end
+
+    body_hash = JSON.parse(@resp.body)
+    @results = body_hash["items"]
+    render 'search'
   end
 end
