0f81d1161246747bb34afbac29b23d54abc36fff
zoebisch <zoebisch@gmail.com>
2018-01-26 12:22:36 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,8 +7,8 @@ class RepositoriesController < ApplicationController
 
     @resp = Faraday.get("https://api.github.com/search/repositories") do |req|
         req.params["q"] = params[:query]
-        req.params["client_id"] = "2f2b3cacc08caf9ba927"
-        req.params["client_secret"] = "b6804c36d70f631daf761e5d028dcef76069de77"
+        req.params["client_id"] = "INSERT ID HERE"
+        req.params["client_secret"] = "INSERT SECRET HERE"
       end
 
       body = JSON.parse(@resp.body)
