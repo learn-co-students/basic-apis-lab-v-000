c2bbbca6fa98b25b621d37fe1cf84ce48d2c2660
zoebisch <zoebisch@gmail.com>
2018-01-26 12:21:25 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 #faraday for low level HTTP
 gem 'faraday'
+gem 'pry'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,27 +1,19 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
-    begin
-      @resp = Faraday.get 'https://api.github.com/search' do |req|
-          req.params['client_id'] = client_id
-          req.params['client_secret'] = client_secret
-          req.params['q'] = 'blah'
-          req.options.timeout = 0
-        end
-        body = JSON.parse(@resp.body)
-        if @resp.success?
-          @venues = body["response"]["venues"]
-        else
-          @error = body["meta"]["errorDetail"]
-        end
 
-      rescue Faraday::ConnectionFailed
-        @error = "There was a timeout. Please try again."
+    @resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+        req.params["q"] = params[:query]
+        req.params["client_id"] = "2f2b3cacc08caf9ba927"
+        req.params["client_secret"] = "b6804c36d70f631daf761e5d028dcef76069de77"
       end
-    render 'search'
+
+      body = JSON.parse(@resp.body)
+      @results = body["items"]
+      render 'search'
   end
+
 end
