e26c10db64cabd610cf3a6b02d12628a77315deb
Amanda Ackerman <30278187+ackerm44@users.noreply.github.com>
2018-02-07 08:55:39 -0500

    Update repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,8 +8,8 @@ class RepositoriesController < ApplicationController
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['q'] = params[:query]
-        req.params['client_id']= 'Iv1.8c7f624055b9b5bc'
-        req.params['client_secret']='c0ec2a25dedfd1c823d1ca64775aa68dca79ef16'
+        req.params['client_id']= ''
+        req.params['client_secret']=''
         req.options.timeout = 20
       end
       body = JSON.parse(@resp.body)
