66171106e65a366dd6972de16292d10526c7ea49
github username <github email address>
2018-03-06 17:12:17 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,4 @@
+
 class RepositoriesController < ApplicationController
 
   def search
@@ -5,5 +6,23 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+    @resp = Faraday.get "https://api.github.com/search/repositories" do |req|
+      req.params['client_id'] = 'Iv1.343baab21f298030'
+      req.params['client_secret'] = 'dd3c78ffd81ac47e4975ee46702e272e3dc46aa1'
+      req.params['q'] = params[:query]
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repo_url = body["items"][0]["html_url"]
+      @desc = body["items"][0]["description"]
+      @name = body["items"][0]["name"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    rescue Faraday::ConnectionFailed
+      @error = "There was an issue sorry."
+    end
+      render "search"
   end
 end
