7082cd7da35b02b5c2ab70e2e92935abe98141ae
Christian Egglin <cegglin@conncoll.edu>
2018-03-16 19:36:24 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,8 +5,8 @@ class RepositoriesController < ApplicationController
   def github_search
     begin
       response = Faraday.get 'https://api.github.com/search/repositories' do |req|
-        req.params['client_id'] = <client_id>
-        req.params['client_secret'] = <client_secret>
+        # req.params['client_id'] = client_id
+        # req.params['client_secret'] = client_secret
         req.params['q'] = params[:query]
         # req.options.timeout = 0
       end
