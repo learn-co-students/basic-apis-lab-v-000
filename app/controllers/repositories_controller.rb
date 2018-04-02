232fd9617af1a7269c5c61ae275ab9961c2b9cd4
Alexander Urizar <urizaraj@gmail.com>
2018-04-02 18:39:05 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,26 @@
 class RepositoriesController < ApplicationController
-
-  def search
-
-  end
+  def search; end
 
   def github_search
+    begin
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      # req.params['client_id'] = client_id
+      # req.params['client_secret'] = client_secret
+      req.params['q'] = params[:query]
+    end
+
+    body = JSON.parse(@resp.body)
+
+    if @resp.success?
+      @repositories = body['items']
+    else
+      @error = body['meta']['errorDetail']
+    end
+
+    rescue Faraday::ConnectionFailed
+      @error = 'There was a timeout. Please try again.'
+    end
+
+    render 'search'
   end
 end
