cdf2c603a4a86a6b1ab3fb10032a704c810cf9a5
Brittany Morris <brittmmendez@gmail.com>
2018-04-02 23:08:01 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,16 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories?q=' do |req|
+      req.params['q'] = params[:query]
+    end
+
+    body_hash= JSON.parse(@resp.body)
+    @repos = body_hash["items"]
+    render :search
   end
+
 end
