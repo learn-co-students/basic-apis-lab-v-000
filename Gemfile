f07023ff1d2326d7f28657ff577b3f897cfd207e
terryblue99 <terryblue@comcast.net>
2018-04-26 19:33:53 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+# Faraday is an HTTP client library that abstracts and standardizes some lower-level HTTP functions
+# and makes it easy to build requests and get responses from an API
+gem 'faraday'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,26 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+  	begin
+	  	@repository_response = Faraday.get 'https://api.github.com/search/repositories' do |req|
+			req.params['client_id'] = '5dd223d3dd78adf88b36'
+			req.params['client_secret'] = '6c2d719cb76f482aa5d099944a7306c5db97b61f'
+			req.params['q'] = params[:query]
+		  end
+		
+		body_hash = JSON.parse(@repository_response.body)
+		if @repository_response.success?
+			@repositories = body_hash["items"]
+		else
+			@error = body["meta"]["errorDetail"]	
+		end
+
+	rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+
+	render 'search'
+	
   end
+
 end
