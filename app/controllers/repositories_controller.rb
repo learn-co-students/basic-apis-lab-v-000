0a8221caf1830a601ee9f8b009cdc3f8c9b2d57b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-03 02:38:57 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,19 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-  end
+     begin
+     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+       req.params['client_id'] = 'client_id'
+       req.params['client_secret'] = 'client_secret'
+       req.params['q'] = params[:query]
+     end
+     body = JSON.parse(@resp.body)
+     if @resp.success?
+       @repositories = body["items"]
+     else
+       @error = "Sorry, the API isn't working at the moment."
+     end
+     render 'search'
+   end
+   end
 end
