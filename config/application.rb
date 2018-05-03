f6f56f0fffc784ad75512aa6e97d62331785c50e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-05-03 15:34:06 +0000

    Done.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module BasicApisLab
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
