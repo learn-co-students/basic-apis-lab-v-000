952f331183446ff2776652b5423aac4b23fdcd89
Lizzie <lizzie.sorin@hotmail.fr>
2018-05-17 22:27:56 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,22 @@
 class RepositoriesController < ApplicationController
 
   def search
-
+  
   end
 
   def github_search
+  	@resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = ENV["CL_ID"]
+      req.params['client_secret'] = ENV["CL_SECRET"]
+      req.params['q'] = params['query']
+  	end
+  	body = JSON.parse(@resp.body)
+	      if @resp.success?
+	        @repos = body["items"]
+	      else
+	        @error = "No Repo found"
+	      end
+ 	render 'search'
   end
+
 end
