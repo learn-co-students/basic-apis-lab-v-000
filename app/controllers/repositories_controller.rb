183992a24f7973ee45d01847fb7a744eaf404c27
Richard Chang <rchang9999@yahoo.com>
2018-06-04 00:34:03 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+
+    response = Faraday.get("https://api.github.com/search/respositories") do |req|
+      req.params["q"] = params[:query]
+      req.params["access_token"]= "690ec16093636a8a7e1ee0f095d380b479ed74fe"
+    end
+
+    @body =JSON.parse(response.body)
+    render '/search'
   end
+
 end
