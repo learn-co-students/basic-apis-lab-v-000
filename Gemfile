30bd58f99049c485186a6cd81679710da3d1c060
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-10 18:27:32 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -44,4 +44,7 @@ gem 'webmock', group: :test
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+  gem 'pry'
 end
+
+gem 'faraday'
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,20 @@
 class RepositoriesController < ApplicationController
-
+  require 'pry'
   def search
 
   end
 
   def github_search
-  end
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = '29bf561992422c84ac7d'
+      req.params['q'] = params[:query]
+      req.params['client_secret'] = 'dcaa8380a067e65103057db20470e24893759dc4'
+    end
+
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @results = body["items"]
+    end
+    render 'search'
+    end
 end
