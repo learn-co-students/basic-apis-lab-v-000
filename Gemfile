296d58742fa4dc96012aef491b264aaba0eeeb9d
jennianelson <jennifernelson118@gmail.com>
2018-06-11 17:14:35 -0500

    Almost there. Add specific repo results to view

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'faraday'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -34,7 +36,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  # gem 'byebug'
+  gem 'pry'
   gem 'rspec-rails'
   gem 'capybara'
   gem 'shoulda-matchers'
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = '6a6ef6d68e50e19fc2e8'
+      req.params['client_secret'] = 'd6836e09fa6d92f654da68d2aa399ca6ff578473'
+      req.params['q'] = params[:query]
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repos = body["items"]
+    else
+      # binding.pry
+      @errors = body["errors"]
+    end
+
+    render 'search'
   end
+
 end
