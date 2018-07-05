cabdd2a38d9e8f28fdfa55e626c8009033d1c0f5
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-07-05 16:13:19 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get '/search', to: 'repositories#search'
   post '/search', to: 'repositories#github_search'
   root 'repositories#search'
+
+
+# theoretical route for user strategy login
+   # get '/auth/github/callback' => ''
+
 end
