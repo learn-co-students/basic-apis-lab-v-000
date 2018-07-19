f56c8e4ac5cbf8e12370f2293b47f864d4e57de4
Jirles <emily.jirles@gmail.com>
2018-07-19 16:06:50 -0400

    Edit JSON.parse arg and success? method in controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,11 +5,19 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.github.com' do |req|
-      req.params['client_id'] = 0dca3f7c89ec46855f8e
-      req.params['client_secret'] = e4691983152af41c47bc3a5eb5422b07fdc04d27
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = '0dca3f7c89ec46855f8e'
+      req.params['client_secret'] = 'e4691983152af41c47bc3a5eb5422b07fdc04d27'
       req.params['q'] = params['query']
+    end
+    # need @error and @repos
+    parsed_json = JSON.parse(@resp.body)
+    if @resp.success?
+      @repos = parsed_json['items']
+    else 
+      @error = "There was an error with your search. Please try again."
     end 
     render 'search'
   end
+
 end
