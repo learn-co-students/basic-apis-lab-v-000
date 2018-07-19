d09bcd7e8a319f29d71c78cafbe6e077e8eccd4d
Jirles <emily.jirles@gmail.com>
2018-07-19 15:36:43 -0400

    Stub out git_hub search action and search results display

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,11 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com' do |req|
+      req.params['client_id'] = 0dca3f7c89ec46855f8e
+      req.params['client_secret'] = e4691983152af41c47bc3a5eb5422b07fdc04d27
+      req.params['q'] = params['query']
+    end 
+    render 'search'
   end
 end
