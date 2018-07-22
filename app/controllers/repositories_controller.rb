8939424ac0a2349346a4fed2b740b7a3cca6ad04
AspenJames <aspenjames@tqca.org>
2018-07-22 14:58:42 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params["q"] = params[:query]
+    end
+    if resp.success?
+      @repos = JSON.parse(resp.body)["items"]
+    else
+      @error = JSON.parse(resp.body)["errors"]
+    end
+    render 'search'
   end
 end
