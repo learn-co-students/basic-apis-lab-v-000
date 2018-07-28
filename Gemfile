dc60196f0dd3c1eb3ca0f1c0fbc0559e8b35e995
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-07-28 09:38:34 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5'
+gem 'rails', '~> 4.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
@@ -44,4 +44,6 @@ gem 'webmock', group: :test
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+    
+  gem 'faraday'
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,19 @@
 class RepositoriesController < ApplicationController
+    require 'faraday'
 
   def search
 
   end
 
-  def github_search
+   def github_search
+    @resp = Faraday.get ('https://api.github.com/search/repositories') do |req|
+      req.params['client_id'] = ''
+
+      req.params['client_secret'] = ''
+      req.params['q'] = params[:query]
+    end
+   body = JSON.parse(@resp.body)
+   @results = body['items']
+  render 'search'
   end
 end
