d7a4f03f65b910066e71bf2b2f6b211f45126da2
Marissa Zhong <mwz2101@columbia.edu>
2018-08-01 13:09:21 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+      req.params["q"] = params[:query]
+      req.params["client_id"] = "078650f1bff6e53dc843"
+      req.params["client_secret"] = "722011c4411c4c4e77cfc0743dc2a1e4cfe474c3"
+    end
+
+    body = JSON.parse(resp.body)
+    @results = body["items"]
+    render :search
   end
 end
