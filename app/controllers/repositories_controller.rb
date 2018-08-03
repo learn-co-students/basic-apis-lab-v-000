41d012a706d9ce2ac344a5e902a2da613485d966
Josh Kramer <josh.kramer.dev@gmail.com>
2018-08-03 00:57:19 -0700

    Update repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,8 +5,8 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    client_id = "5e8749bb1ef294967a1b"
-    secret = "130d8503fedc02a613f11cac6d8ebf95670829b2"
+    client_id = ""
+    secret = ""
 
     resp = Faraday.get('https://api.github.com/search/repositories') do |req|
       req.params["q"] = params[:query]
