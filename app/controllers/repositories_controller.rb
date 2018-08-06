5fd7456012b40d95cda1b592aad696f8cff28f3d
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-08-06 20:37:49 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,15 +6,13 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] = ENV['CLIENT_SECRET']
+      req.params['q'] = params[:query]
     end
  
     body_hash = JSON.parse(@resp.body)
-    @venues = body_hash["response"]["venues"]
+    @results = body_hash["items"]
     render 'search'    
   end
 end
