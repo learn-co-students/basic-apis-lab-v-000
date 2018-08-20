429daa086da0f46250be77a8a960af705ef56ab0
Maverick Tesini <mavericktesini@gmail.com>
2018-08-20 05:44:02 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,15 +1,11 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories?' do |req|
-      # @resp = Faraday.get 'https://api.github.com/search/repositories?q=' do |req|
-        # req.params['client_id'] = client_id
-        # req.params['client_secret'] = client_secret
         req.params['q'] = params[:query]
       end
 
