a500af3d3c3d82896a4179352d006a193cf9014a
Joseph Parker <jparker010190@gmail.com>
2018-09-01 17:54:33 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,21 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'v1.c5f032e339de8903'
+      req.params['client_secret'] = '914cf3f0c0c5f416a17ec435261283d74f4d8332'
+      req.params['q'] = params[:query]
   end
+  body = JSON.parse(@resp.body)
+  if @resp.success?
+    @results = body["items"]
+  else
+    @error = body["meta"]["errorDetail"]
+  end
+  rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+  end
+  render 'search'
+end
 end
