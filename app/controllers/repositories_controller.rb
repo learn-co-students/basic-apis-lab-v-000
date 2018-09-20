a8b846a2ad977767bc3b9de9bbaefdc849b579ef
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-20 21:04:18 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,17 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = 'client_id'
+      req.params['client_secret'] = 'client_secret'
+    end
+    search_hash = JSON.parse(@resp.body)
+  end
+
+  private
+
+  def repo_params
+    params.require(:query).permit(:query)
   end
 end
