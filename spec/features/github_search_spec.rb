5444fd945cea4229c5f31caa1c4b86064313c402
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-22 06:10:31 -0400

    Finicky. ATP.

--- spec/features/github_search_spec.rb
+++ spec/features/github_search_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "Searching Github", type: :feature do
 
   it "displays the right data" do
-    stubbed_request = stub_request(:get, "https://api.github.com/search/repositories").with(query: hash_including({q: "tetris"})).to_return(body: repository_response)
+    stubbed_request = stub_request(:get, "https://api.github.com/search/repositories").with(query: hash_including({q: "tetris+language:assembly", sort: "stars", order: "desc"})).to_return(body: repository_response)
 
     visit '/search'
-    fill_in 'query', with: "tetris"
+    fill_in 'query', with: "tetris+language:assembly"
     click_button "Search Github!"
 
     expect(page).to have_link("https://github.com/dtrupenn/Tetris", href: "https://github.com/dtrupenn/Tetris")
