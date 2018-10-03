3e7679af2fc0f810927b752809908ab580745958
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-03 20:56:30 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,5 +8,7 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = '6c087bbc97f41cac6dc6'
       req.params['client_secret'] = '28d92a155e7ec7b204ec058e40a856e929e062eb'
+    end 
+      render 'search'
   end
 end
