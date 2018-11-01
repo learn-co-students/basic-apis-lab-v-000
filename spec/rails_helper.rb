aa749418d34594769aeb058158dbe1eaf863bcf3
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-01 12:55:46 +0000

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,8 @@ require 'support/github_response.rb'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-WebMock.disable_net_connect!(:allow_localhost => true)
+WebMock.allow_net_connect!
+#WebMock.disable_net_connect!(:allow_localhost => true)
 
 RSpec.configure do |config|
   config.include GithubHelpers
