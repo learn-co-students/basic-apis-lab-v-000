43cc0489c17f015c70feefbfc075041ce47eccd6
Shirley Collins <shirley@m104.us>
2018-11-06 09:08:14 -0800

    remove ids

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,8 +5,8 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    client_id = '9dc0b36308dee688194e'
-    client_secret = 'a805b2c8c164761517b682f7874b36727ff8bea9'
+    client_id = 'client_id'
+    client_secret = 'client_secret'
 
     @response = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = client_id
