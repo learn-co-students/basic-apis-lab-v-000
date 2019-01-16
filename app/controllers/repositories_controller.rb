f5d1c3dc4dab3824754654601f6732d63528893a
Kachel Wittig <kjwittig@gmail.com>
2019-01-16 15:46:56 -0600

    Do not commit your secrets

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,20 @@
+require 'pry'
+
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    # GET /search/repositories
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = ''
+      req.params['client_secret'] = ''
+    end
+    body_hash = JSON.parse(@resp.body)
+    @repos = body_hash['items']
+    render 'search'
   end
+
 end
