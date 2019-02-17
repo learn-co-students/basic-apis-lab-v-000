cdc1807261705781c20219254961dd84607e93ad
epic0448 <samuelogundiran@Yahoo.com>
2019-02-16 19:27:16 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# adding faraday
+gem 'faraday'
+gem 'pry'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -45,9 +48,7 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
-  # adding faraday
-  gem 'faraday'
-
-
+  
+ 
   gem 'dotenv-rails', '~> 2.1', '>= 2.1.1'
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class RepositoriesController < ApplicationController
 
   def search
@@ -11,7 +13,7 @@ class RepositoriesController < ApplicationController
     @repos = []
 
     
-      @repo = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         #  for the client_id and Client_secret, you are expect to put the real
         # client_Id and client_secret into the code...
         req.params['client_id'] = client_id
@@ -22,6 +24,7 @@ class RepositoriesController < ApplicationController
           body = JSON.parse(@resp.body)
           if @resp.success?
             @repos = body["items"]
+
           else
             @error = "errors! oh my" #body["meta"]["errorDetail"]
           end
@@ -29,6 +32,9 @@ class RepositoriesController < ApplicationController
    end
 
  end
+
+
+#  https://blog.phusion.nl/2017/10/13/why-ruby-app-servers-break-on-macos-high-sierra-and-what-can-be-done-about-it/
 
 
 
