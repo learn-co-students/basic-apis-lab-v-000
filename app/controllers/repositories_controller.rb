219c78ab270e7c4ab0af04b572726af1a201593f
Bryan Hooper <bthooper@gmail.com>
2019-02-18 18:27:48 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,23 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    begin
+      @resp = Faraday.get('https://api.github.com/search/repositories') do |req| 
+        req.params['q'] = params[:query] 
+      end
+
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @repos = body['items']
+      else
+        @error = body['message']
+      end
+    rescue Faraday::ConnectionFailed
+      @error = "Your connection failed."
+    end
+    render 'search'
   end
 end
