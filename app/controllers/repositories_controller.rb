f0de3e032d4292824255d0d470e7a2fe39614513
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-02-19 18:16:32 -0800

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,16 +5,18 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    # @resp =  Faraday.get 'https://api.github.com/search/repositories' do |req|
-    #   req.params['q'] = params[:query]
-    #   req.params['client_id'] = 'Iv1.835814dca52676a7'
-    #   req.params['client_secret'] = '3c77a97eeb69a5ad82d3ea12a60cd4c750897053'
-    #   req.params['query'] = 'pokemon'
-    # end
+    @resp =  Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = "Iv1.835814dca52676a7"
+      req.params['client_secret'] = "3c77a97eeb69a5ad82d3ea12a60cd4c750897053"
+      req.params['query'] = "tetris"
+    end
 
-    # body_hash = JSON.parse(@resp.body)
-    # @repos = body_hash["items"]
+    # binding.pry
 
-    # render 'search'
+    body_hash = JSON.parse(@resp.body)
+    @repos = body_hash["items"]
+
+    render 'search'
   end
 end
