7d37ffb0843cc2f5ce6050f92620c641a1899722
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2019-03-05 17:05:38 -0500

    Update repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,6 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = 'Iv1.6d733d92486e177d'
-      req.params['client_secret'] = 'f5c30da765be77baeb4e94254a2647776e1e0029'
       req.params['q'] = params[:query]
       req.params['per_page'] = '100'
     end
