46c12078d70ff3403fd87679a9122d199d25ba1a
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-03-10 19:29:43 -0600

    Passed Tests

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'Iv1.86ca8988611de93b'
+      req.params['client_secret'] = 'Iv1.86ca8988611de93b'
+      req.params['q'] = params[:query]
+    end
+
+    body_hash = JSON.parse(@resp.body)
+    @repositories = body_hash['items']
+    render 'search'
   end
 end
