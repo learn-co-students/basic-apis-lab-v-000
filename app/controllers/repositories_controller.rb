e1660f350c53f3cddc91cf2d78a4fcb82a91cc36
Saad Ahmed <dropheaven@live.com>
2019-03-22 07:41:50 -0400

    removed id and secret

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,8 +6,8 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = '93dfe7cb3b73e2badd5d'
-      req.params['client_secret'] = 'b64f7249fcc45f84ab27c6cf6da921b05da41a05'
+      req.params['client_id'] = ''
+      req.params['client_secret'] = ''
       req.params['q'] = params[:query]
     end
 
