fc67c030fa443d85c950e35567399a8290e9a077
Catherine Bronzo <cbronzo@mac.com>
2019-03-28 03:59:48 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,17 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    root_path = 'https://api.github.com/'
+
+     @response = Faraday.get root_path + 'search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = ''
+      req.params['secret'] = ''
+    end
+
+     body_hash = JSON.parse(@response.body)
+    @results = body_hash['items']
+
+     render :search
   end
 end
