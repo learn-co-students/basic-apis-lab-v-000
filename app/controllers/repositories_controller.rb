06b06bfa32e5f5899a90f3d3a43e7bb923bb0db8
Erin Green <erin.y.green@gmail.com>
2019-06-06 18:31:28 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,20 +1,21 @@
 class RepositoriesController < ApplicationController
 
   def search
-    @query = params[:query]
+    user_input = params[:query]
+    @query = user_input.split(" ").join("+")
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
     req.params['client_id'] = 'Iv1.6fb249e0fdd7b2ac'
     req.params['client_secret'] = 'b41247c4dbc2e68344a15f4fdfc37e1331fe4035'
-    req.params['q'] = '@query'
+    req.params['q'] = `#{@query}`
 
   end
 
     body = JSON.parse(@resp.body)
     if @resp.success?
-      @repositories = body["response"]["venues"]
+      @repositories = body["response"]["items"]
     else
       @error = body["meta"]["errorDetail"]
     end
