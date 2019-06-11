3a502a1ac144c4b93b7efb90615afaa304afe1fd
Grace Choi <oiograceoio@gmail.com>
2019-06-11 18:10:35 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,15 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+  	@resp = Faraday.get'https://api.github.com/search/repositories' do |req|
+  		req.params['q'] = params[:query]
+  	end
+  	body = JSON.parse(@resp.body)
+  	if @resp.success?
+  		@repos = body["items"]
+  	else
+  		@error = "Sorry! Try again."
+  	end
+  	render 'search'
   end
 end
