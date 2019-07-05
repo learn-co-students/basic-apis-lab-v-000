a347b83b9aa4d5f901ceb4076e7dad93041587ed
chrissygonzalez <chrissygonzalez@gmail.com>
2019-07-05 13:22:47 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,16 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'bca24975b7c654111711'
+      req.params['client_secret'] = 'b2a05bc1c3ef65c379277a0eb98a1980845c370b'
+      req.params['q'] = params[:query]
+    end
+
+    body_hash = JSON.parse(@resp.body)
+    # binding.pry
+    @repos = body_hash["items"]
+
+    render 'search'
   end
 end
