0c09a0c1f61844074deb9966c3f760730f214a3d
Jonathan Luk <jluk87@gmail.com>
2019-08-23 20:43:23 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/search', to: 'repositories#search'
   post '/search', to: 'repositories#github_search'
+  get '/github/callback', to: 'repositories#github_search'
   root 'repositories#search'
 end
