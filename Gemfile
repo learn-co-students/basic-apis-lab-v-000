7de602703c4d4e80b7c0b8c983af4a7a3c3c4f15
Robin Doirin <robindoirin@gmail.com>
2019-09-18 15:44:56 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'faraday'
 gem 'rails-controller-testing'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'dotenv'
+gem 'dotenv-rails'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = ENV['GITHUB_CLIENT']
+      req.params['client_secret'] = ENV['GITHUB_SECRET']
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @results = body["items"]
+    else
+      @error = body["meta"]["message"]
+    end
+    #rescue Faraday::ConnectionFailed
+    #  @error = "There was a timeout. Please try again."
+    #end
+    render 'search'
   end
 end
