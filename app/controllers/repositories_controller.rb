8aac4767c3e706b8525e8c058727d5d6beec0512
RonSala <ron.sala@gmail.com>
2019-10-14 01:03:36 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -13,7 +13,7 @@ class RepositoriesController < ApplicationController
     end
     body = JSON.parse(@resp.body)
     if @resp.success?
-      @repos = body
+      @repos = body["items"]
     end
 
     render 'search'
