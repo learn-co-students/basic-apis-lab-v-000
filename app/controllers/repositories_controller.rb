caa2c938df3779eccbf3beebced6b221c7245c0a
Kat Tochowicz <ktochow1@binghamton.edu>
2020-02-04 22:21:48 -0500

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,23 +7,25 @@ class RepositoriesController < ApplicationController
   def github_search
     # binding.pry
     begin
-    @resp = Faraday.get 'https://api.github.com/search/repositories?q=params[query]/whatever?client_id=Iv1.821d8a715066a841&client_secret=11d2fc14364ac7458dcf7fa738806faa01dacd7b' do |req|
+    @resp = Faraday.get("https://api.github.com/search/repositories?q=params[query]/whatever?client_id=Iv1.821d8a715066a841&client_secret=11d2fc14364ac7458dcf7fa738806faa01dacd7b") do |req|
       req.params['client_id'] = 'Iv1.821d8a715066a841'
       req.params['client_secret'] = '11d2fc14364ac7458dcf7fa738806faa01dacd7b'
-      req.params['q'] = params['query']
+      req.params['q'] = params[:query]
     end
 
-    # body = JSON.parse(@resp.body)
+    body = JSON.parse(@resp.body)
+    @results = body["items"]
     # if @resp.success?
-    #   @venues = body["response"]["venues"]
-    # else
-    #   @error = body["meta"]["errorDetail"]
-    # end
+    #    @venues = body["response"]["venues"]
+    #  else
+    #    @error = body["meta"]["errorDetail"]
+    #  end
 
-  rescue Faraday::ConnectionFailed
-    @error = "There was a timeout. Please try again."
-
+  # rescue Faraday::ConnectionFailed
+  #   @error = "There was a timeout. Please try again."
+  #
   end
+  render :search
 end
 
 end
